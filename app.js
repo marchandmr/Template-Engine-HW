295e0708ec5b69210c0a1b4229455f47d09de74e
Matthew Marchand <marchandmr@icloud.com>
2020-12-23 21:14:17 -0600

    page is working

--- app.js
+++ app.js
@@ -34,35 +34,110 @@ const managerInfo = () =>
                 name: 'officeNumber',
                 message: 'What is the managers office number?'
             }
-        ]);
+        ])
+        .then((data) => {
+            const manager = new Manager(data.manager, data.managerID, data.managerEmail, data.officeNumber)
+            employees.push(manager);
+            addEmployee();
+        })
 
-managerInfo().then((data) => {
-    const manager = new Manager(data.manager, data.managerID, data.managerEmail, data.officeNumber)
-    employees.push(manager);
+function addEmployee() {
+    inquirer.prompt(
+        [
+            {
+                type: 'list',
+                name: 'type',
+                message: ' add an employee?',
+                choices: [
+                    'engineer',
+                    'intern',
+                    'manager',
+                    'All Done!'
+                ]
+            }])
+        .then((data) => {
+            if (data.type === 'intern') {
+                internInfo()
+            }
+            else if (data.type === 'engineer') {
+                engineerInfo();
+            }
+            else if (data.type === 'manager') {
+                managerInfo();
+            }
+            else if (data.type === 'All Done!') {
+                fs.writeFileSync(outputPath, render(employees), "utf-8");
+                console.log(employees);
+            }
 
-});
+
+        })
+}
+
+const internInfo = () =>
+    inquirer.prompt(
+        [
+
+            {
+                type: 'input',
+                name: 'intern',
+                message: 'What is your interns name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'employee ID #?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'add the interns email address'
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'add interns school'
+            }
+        ])
+        .then((data) => {
+            const intern = new Intern(data.intern, data.id, data.email, data.school)
+            employees.push(intern);
+            addEmployee();
+
+        })
+
+const engineerInfo = () =>
+    inquirer.prompt(
+        [
+
+            {
+                type: 'input',
+                name: 'engineer',
+                message: 'What is your engineers name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'employee ID #?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'add the interns email address'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'add the engineers github page'
+            }
+        ])
+        .then((data) => {
+            const engineer = new Engineer(data.engineer, data.id, data.email, data.github)
+            employees.push(engineer);
+            addEmployee();
+        }
+        )
+
+managerInfo();
 
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
